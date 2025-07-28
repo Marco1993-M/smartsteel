6fffe1328a62ca937285502ec74a2fd708ca6ed2
Marco1993-M <136054712+Marco1993-M@users.noreply.github.com>
2025-07-28 16:32:22 +0200

    Add files via upload

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+// next-sitemap.config.js
+module.exports = {
+  siteUrl: 'https://www.smartsteel.co.za',
+  generateRobotsTxt: true,
+  sitemapSize: 5000,
+  changefreq: 'monthly',
+  priority: 0.7,
+};
