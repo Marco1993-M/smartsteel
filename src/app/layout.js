d1689f4747c9a401df9d9759f31e8e76cdd8191a
Marco1993-M <136054712+Marco1993-M@users.noreply.github.com>
2025-08-11 13:19:06 +0200

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -131,7 +131,7 @@ export default function RootLayout({ children }) {
           dangerouslySetInnerHTML={{ __html: JSON.stringify(organizationJsonLd) }}
         />
 
-        <AnnouncementBanner />
+       {/* <AnnouncementBanner /> */}
         <Navbar />
         {children}
         <Footer />
