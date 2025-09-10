1fdd20e175e59394b15fe863bac61588d1e2ab0a
Marco1993-M <136054712+Marco1993-M@users.noreply.github.com>
2025-09-10 14:38:56 +0200

    Update KanbanPage.js

--- src/components/KanbanPage.js
+++ src/components/KanbanPage.js
@@ -35,7 +35,7 @@ export default function KanbanPage() {
 
   return (
     <div className="p-6">
-      <div className="flex justify-between items-center mt-16 mb-6">
+      <div className="flex justify-between items-center mt-6 mb-6">
         <h1 className="text-2xl font-bold">Smart Steel Leads Centre</h1>
         <button
           className="bg-red-600 text-white px-4 py-2 rounded hover:bg-red-700"
