0a9f95963e1ed0d8dea69e948f991a1c4914887b
Marco1993-M <136054712+Marco1993-M@users.noreply.github.com>
2025-10-07 10:06:19 +0200

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,8 +2,14 @@
 module.exports = {
   siteUrl: 'https://www.smartsteel.co.za',
   generateRobotsTxt: true,
-  generateIndexSitemap: false, // ✅ This is important
   sitemapSize: 5000,
   changefreq: 'monthly',
   priority: 0.7,
+  exclude: [
+    '/news-events',
+    '/projects',
+    '/case-studies/school-expansion',
+    '/case-studies/retail-renovation',
+    '/case-studies/eco-home',
+  ],
 };
