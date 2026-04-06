28bdd1685c21ef77505c81f5febcd7981f66e05d
Marco1993-M <136054712+Marco1993-M@users.noreply.github.com>
2026-04-06 13:25:08 +0200

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -55,7 +55,8 @@ export default function Footer() {
            <p className="text-sm text-gray-300 mb-2"> <a href="/centurion-warehouses" className="hover:text-[#da1a33]">Centurion</a></p>
            <p className="text-sm text-gray-300 mb-2"> <a href="/midrand-warehouses" className="hover:text-[#da1a33]">Midrand</a></p>
            <p className="text-sm text-gray-300 mb-2"> <a href="/pretoria-warehouses" className="hover:text-[#da1a33]">Pretoria</a></p>
-          <p className="text-sm text-gray-300 mb-2"> <a href="/roodepoort-warehouses" className="hover:text-[#da1a33]">Roodepoort</a></p>
+           <p className="text-sm text-gray-300 mb-2"> <a href="/roodepoort-warehouses" className="hover:text-[#da1a33]">Roodepoort</a></p>
+           <p className="text-sm text-gray-300 mb-2"> <a href="/middelburg-warehouses" className="hover:text-[#da1a33]">Middelburg</a></p>
             <div className="grid md:grid-cols-3 gap-4 text-sm">
 
         
