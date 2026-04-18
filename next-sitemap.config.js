62393da3af84fdf2eb9a692ca05741713bbc59b3
Marco1993-M <136054712+Marco1993-M@users.noreply.github.com>
2026-04-18 19:27:29 +0200

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,29 @@
-User-agent: *
-Allow: /
-Disallow: /login
-Disallow: /kanban
-Disallow: /api/
-Sitemap: https://www.smartsteel.co.za/sitemap.xml
+// next-sitemap.config.js
+module.exports = {
+  siteUrl: 'https://www.smartsteel.co.za',
+  generateRobotsTxt: true,
+  sitemapSize: 5000,
+  changefreq: 'monthly',
+  priority: 0.7,
+  exclude: [
+    '/api',
+    '/api/*',
+    '/kanban',
+    '/login',
+    '/news-events',
+    '/projects',
+    '/case-studies/school-expansion',
+    '/case-studies/retail-renovation',
+    '/case-studies/eco-home',
+  ],
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/api', '/api/*', '/kanban', '/login'],
+      },
+    ],
+    additionalSitemaps: ['https://www.smartsteel.co.za/sitemap.xml'],
+  },
+};
