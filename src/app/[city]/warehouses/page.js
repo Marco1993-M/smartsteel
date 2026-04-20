2c5aad41f07ac78a43ff101ff1817eb070b38089
Marco1993-M <136054712+Marco1993-M@users.noreply.github.com>
2026-04-20 19:08:12 +0200

    Add files via upload

---
+++ src/app/[city]/warehouses/page.js
@@ -0,0 +1,31 @@
+import { notFound } from "next/navigation";
+import RegionWarehousePageClient from "../../warehouse-regions/RegionWarehousePageClient";
+import {
+  buildRegionWarehouseMetadata,
+  getRegionWarehouseCitySlugs,
+  getRegionWarehouseConfig,
+} from "../../warehouse-regions/regionWarehouseData";
+
+export const dynamicParams = false;
+
+export function generateStaticParams() {
+  return getRegionWarehouseCitySlugs().map((city) => ({ city }));
+}
+
+export async function generateMetadata({ params }) {
+  const { city } = await params;
+  const config = getRegionWarehouseConfig(city);
+  if (!config) return {};
+  return buildRegionWarehouseMetadata(city);
+}
+
+export default async function Page({ params }) {
+  const { city } = await params;
+  const config = getRegionWarehouseConfig(city);
+
+  if (!config) {
+    notFound();
+  }
+
+  return <RegionWarehousePageClient citySlug={city} />;
+}
