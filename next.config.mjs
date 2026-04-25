e5208c6a69a947716d9bb351512940f241335bfe
Marco1993-M <136054712+Marco1993-M@users.noreply.github.com>
2026-04-25 17:01:19 +0200

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  turbopack: {
+    root: '/Users/marcogerritsen/Desktop/Dev Files/smartsteel',
+  },
+  outputFileTracingIncludes: {
+    "/api/estimates/*/pdf": [
+      "./node_modules/@sparticuz/chromium/**/*",
+    ],
+  },
+};
 
 export default nextConfig;
