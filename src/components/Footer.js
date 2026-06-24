1fb4e7b9b447d2b9b8a47040eff29bb5abb02f74
Marco1993-M <136054712+Marco1993-M@users.noreply.github.com>
2026-06-24 09:32:30 +0200

    Add files via upload

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -33,6 +33,7 @@ export default function Footer() {
           <ul className="space-y-2 text-sm text-gray-300">
             <li><Link href="/warehouses" className="hover:text-[#da1a33]">Projects &amp; Systems</Link></li>
             <li><Link href="/products" className="hover:text-[#da1a33]">Products &amp; DIY Systems</Link></li>
+            <li><Link href="/steel-fabrication-installation" className="hover:text-[#da1a33]">Steel Fabrication &amp; Installation</Link></li>
             <li><Link href="/resources" className="hover:text-[#da1a33]">Technical Resources</Link></li>
             <li><Link href="/resources" className="hover:text-[#da1a33]">Installation Guides</Link></li>
             <li><Link href="/warehouse-builder" className="hover:text-[#da1a33]">Warehouse Builder</Link></li>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -188,6 +188,7 @@ export default function Navbar() {
             <div>
               <h3 className="text-xl font-semibold text-[#da1a33] mb-4">Fabricators</h3>
               <ul className="space-y-2">
+                <li><Link href="/steel-fabrication-installation" className="hover:underline">Steel Fabrication &amp; Installation</Link></li>
                 <li><Link href="/product-advantages" className="hover:underline">Product Advantages</Link></li>
                 <li><Link href="/resources" className="hover:underline">Technical Resources</Link></li>
               </ul>
