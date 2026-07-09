2772bc41078755af0552f528c031c95a2e34727e
Marco1993-M <136054712+Marco1993-M@users.noreply.github.com>
2026-07-09 17:06:12 +0200

    Add files via upload

--- src/app/os/crm/page.js
+++ src/app/os/crm/page.js
@@ -1,4 +1,4 @@
-import CrmWorkspace from "@/components/crm/CrmWorkspace"
+import CrmWorkspace from "../../../components/crm/CrmWorkspace.js"
 
 export default function SmartSteelOsCrmPage() {
   return <CrmWorkspace mode="os" />
