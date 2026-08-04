32760021a997fe57cf4da7320aea21c9f5afdb55
Marco1993-M <136054712+Marco1993-M@users.noreply.github.com>
2026-08-04 17:20:46 +0200

    Add files via upload

--- src/lib/warehouseBuilderStore.js
+++ src/lib/warehouseBuilderStore.js
@@ -1,5 +1,29 @@
 import { create } from "zustand"
 
+export const WAREHOUSE_SHEETING_COLORS = [
+  { value: "fish-eagle-white", label: "Fish Eagle White", hex: "#f2f1e8" },
+  { value: "white-lion", label: "White Lion", hex: "#ffffff" },
+  { value: "sandstone-beige", label: "Sandstone Beige", hex: "#c8b99c" },
+  { value: "gemsbok-sand", label: "Gemsbok Sand", hex: "#b79b72" },
+  { value: "aloe-green", label: "Aloe Green", hex: "#718167" },
+  { value: "traffic-green", label: "Traffic Green", hex: "#2f5b3e" },
+  { value: "uhmlanga-wave", label: "Uhmlanga Wave", hex: "#287b88" },
+  { value: "kingfisher-blue", label: "Kingfisher Blue", hex: "#315f82" },
+  { value: "azure-blue", label: "Azure Blue", hex: "#4a79a8" },
+  { value: "dove-grey", label: "Dove Grey", hex: "#a7adb1" },
+  { value: "charcoal-grey", label: "Charcoal Grey", hex: "#505457" },
+  { value: "dark-dolphin", label: "Dark Dolphin", hex: "#30383e" },
+  { value: "kalahari-red", label: "Kalahari Red", hex: "#8d4036" },
+  { value: "buffalo-brown", label: "Buffalo Brown", hex: "#5c4234" },
+]
+
+export const WAREHOUSE_OPENING_FACE_OPTIONS = [
+  { value: "front", label: "Front" },
+  { value: "rear", label: "Rear" },
+  { value: "left", label: "Left side" },
+  { value: "right", label: "Right side" },
+]
+
 export const DEFAULT_WAREHOUSE_BUILDER_STATE = {
   productType: "LSF Warehouse",
   width: 10,
@@ -13,7 +37,10 @@ export const DEFAULT_WAREHOUSE_BUILDER_STATE = {
   enclosureType: "roof_only",
   rollerDoorCount: 0,
   garageDoorOpeningType: "single",
+  rollerDoorFace: "front",
   pedestrianDoorCount: 0,
+  pedestrianDoorFace: "rear",
+  sheetingColor: "kalahari-red",
   steelFinish: "Galv",
   gableMode: "fully_enclosed",
   deliveryRequired: false,
